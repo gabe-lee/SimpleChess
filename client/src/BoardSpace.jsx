9d285a663ee2a23f83b13d9cb80f3d2acaaadd9c
Gabe-Lee <gla.ander@gmail.com>
2019-11-22 09:28:29 -0600

    Format board styles

--- client/src/BoardSpace.jsx
+++ client/src/BoardSpace.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import GamePiece from './GamePiece.jsx';
 
-export default BoardSpace = props => (
+const BoardSpace = props => (
   <div className={`board-space color-${props.color}`}>
-    <GamePiece type={props.pieceType} />
+    <GamePiece type={props.piece} />
   </div>
 )
+
+export default BoardSpace;
--- client/src/GameBoard.jsx
+++ client/src/GameBoard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BoardSpace from './BoardSpace.jsx';
 
-export default GameBoard = props => (
+const GameBoard = props => (
   <div id="game-board">
-    {[8,7,6,5,4,3,2,1].map(row => {
-      [1,2,3,4,5,6,7,8].map(col => <BoardSpace color={(row+col) % 2} piece={props.boardState[row][col]} key={`${col}${row}`}/>)
+    {[0,1,2,3,4,5,6,7].map(row => {
+      return [0,1,2,3,4,5,6,7].map(col => <BoardSpace color={(row+col) % 2} piece={props.boardState[row][col]} key={`${row}${col}`}/>)
     })}
   </div>
 )
+
+export default GameBoard;
